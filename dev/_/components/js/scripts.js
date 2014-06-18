85392d5422035e5d5604db51c071d29e0fa834ba
Ben Xie <bxie1@swarthmore.edu>
2014-06-18 20:35:42 +0000

    Some basic backbone code added to scripts.js

--- dev/_/components/js/scripts.js
+++ dev/_/components/js/scripts.js
@@ -1,1 +1,38 @@
-// whatever javascript files you place in this folder will be concatenated to a single file in the js library
+// whatever javascript files you place in this iFEF `folder will be concatenated to a single file in the js library
+
+function test(arg){
+	var output = arg || 'fire';
+	console.log(output);
+}
+
+var AV = {};
+
+
+AV.routes = Backbone.Router.extend({
+
+	routes: {
+		'': 'index'
+	},
+	initialize:function(){
+		test('init');
+	},
+	index: function(){
+		this.model = new AV.model;
+		this.model.save();	
+	}
+});
+
+AV.model = Backbone.Model.extend({
+	url: 'localhost:8182/juxta/source',
+	data:{
+		
+	}
+});
+
+AV.view = Backbone.View.extend({});
+
+var readysetgo = new AV.routes();
+
+$(function(){
+  Backbone.history.start(); 
+});
