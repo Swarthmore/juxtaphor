933088b1d59b619aee91bb90236e45b277755c37
Ben Xie <bxie1@swarthmore.edu>
2014-06-23 20:34:01 +0000

    make sourceView basic functionality

--- dev/_/components/js/sourceView.js
+++ dev/_/components/js/sourceView.js
@@ -2,7 +2,20 @@
 AV.SourceView = Backbone.View.extend({
 	initialize: function(){
 		console.log("Source view init");
-	}
+	},
+	render: function(){
+		//compile the template using underscore
+		var template = _.template( $("#search_template").html(), {} );
+		//Load the compiled HTML into the backbone "el"
+		this.$el.html( template );
+	},
+	events: {
+            "click input[type=button]": "doSearch"  
+        },
+        doSearch: function( event ){
+        // Button clicked, you can access the element that was clicked with event.currentTarget
+        	alert( "Search for " + $("#search_input").val() );
+        }
 
 });
 var source_view = new SourceView();
