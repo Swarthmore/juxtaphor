6bf5749d14f1abf46826f8db28caed3fc6d9bfc8
thedrew00582 <thedrew00582@gmail.com>
2014-06-24 17:47:51 +0000

    removed a few errors

--- dev/_/components/js/sourceModel.js
+++ dev/_/components/js/sourceModel.js
@@ -1,34 +1,34 @@
 // Source Model
 AV.source = Backbone.Model.extend({
-<<<<<<< HEAD
-	url: 'php/redirect.php',
-=======
+
+
+
 	url: 'php/redirect.php/source',
->>>>>>> db2857fd9cdd2eb9df3ba70fe63b5a12bae7e9da
+
 	defaults: {
 		name: '',
 		type: 'raw',
 		contentType: 'txt',
 		data: ''
-<<<<<<< HEAD
-	}
-=======
+
+
+
 	},
->>>>>>> db2857fd9cdd2eb9df3ba70fe63b5a12bae7e9da
+
 });
 
 
 // Source Model Tests
 
-<<<<<<< HEAD
+
 test_source = new AV.source({
-    name: 'Andrews Potato Chips',
+    name: 'Potato Chips',
     type: 'raw',
     contentType: 'txt',
     data: "No matter where it is, you'll always find a bag around. At a bar, or"+
          " a picnic. Even a baseball ground!"
 });
-test_source.set({name: "Andrew is Cool"});
+
 test_source.save( {
         success: function () {
             alert('success');
@@ -37,7 +37,7 @@ test_source.save( {
             alert('everything is terrible');
         }       
     });
-=======
+
 
 // test_source = new AV.source({
 //     name: 'The Wind and the Rain',
@@ -59,7 +59,7 @@ other_test = new AV.source();
 other_test.url = 'php/redirect.php/source/15';
 other_test.fetch();
 
->>>>>>> db2857fd9cdd2eb9df3ba70fe63b5a12bae7e9da
+
 
 
 // Source Model End
