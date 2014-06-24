64736477650750b47ab5e1a6fe2658181b9c130d
bulbil <n@nabilk.com>
2014-06-24 13:21:18 -0400

    refactor redirect.php

--- dev/php/redirect.php
+++ dev/php/redirect.php
@@ -2,10 +2,10 @@
 // a small utility to redirect the backbone's model saves to the API (avoids cross domain nonsense)
 include 'httpful.phar';
 
-$fpath = '../log/log.txt';
-if(isset($_GET['log']) && $_GET['log'] = 1) {
+define('LOG_PATH', '../log/log.txt');
+define('BASE_URL', 'http://54.88.3.200:8182/juxta');
 
-        $f = fopen($fpath, 'r');
+if(isset($_GET['log']) && $_GET['log'] = 1) {
         echo '<h1> LOG FILE </h1>';
         echo '<hr/>';
         echo '<br /><br />';
@@ -14,64 +14,54 @@ if(isset($_GET['log']) && $_GET['log'] = 1) {
 
         $log = file_get_contents($fpath);
         echo $log;
-        fclose($f);
-
 } else {
+       $f = fopen(LOG_PATH,'a');
 
-        $baseURL = 'http://54.88.3.200:8182/juxta';
-        $route = array('/source', '/transform');
-        $f = fopen($fpath,'a');
+        $verb = strtolower($_SERVER['REQUEST_METHOD']);
+        $path_array = explode('/',$_SERVER['REQUEST_URI']);
+        $id = (int) end($path_array);
+        $route = ($id !== 0) ? prev($path_array) . '/' .  array_pop($path_array) : '/' . array_pop($path_array);
+        $json_string = file_get_contents('php://input');
 
-        fwrite($f, '<h2>' . time() . '</h2>');
+        fwrite($f, '<h2>' . date(DATE_RFC2822,time()) . '</h2>');
         fwrite($f, "<hr/><pre>\n\n");
-
-        $json_string = file_get_contents('php://input');  
+        fwrite($f, $_SERVER['REQUEST_URI']);
         fwrite($f, $json_string);
-
-        $json_array = json_decode($json_string,true);
-        $json_write = print_r($json_array, true);
-        
-        fwrite($f, $json_write);
-        fwrite($f, "\n\n");
-	
-	$verb = strtolower($_SERVER['REQUEST_METHOD']);
-	$path_array = explode('/',$_SERVER['REQUEST_URI']);
-	$route = (is_int(end($path_array))) ? prev($path_array) . '/' .  array_pop($path_array) : '/' . array_pop($path_array);
-	
-        fwrite($f, $verb . ' / ' . $route);
         fwrite($f, "\n\n");
 
-	try{      
+		$args = array (
+		        $verb,
+		        $route,
+		        $json_string,
+		        $f
+		);
+
 		switch($verb){
-		case('post'): req($route,$verb); break;
-		case('get'): req($route,$verb); break;
+		case('post'): jws_request($args); break;
+		case('get'): jws_request($args); break;
 		case('put'): break;
 		case('delete'): break;
 		}
-	
-
-		function req($route, $verb){
-			$call = \Httpful\Request::$verb($baseURL . $route);
-			if($verb == 'post') { $call->sendsJson()->body('[' . $json_string . ']'); }
-			$call->send();	
-                	$body = print_r($call, true);
-                	fwrite($f, $body);
-			echo $call->raw_headers;
-		}
-		//$call = \Httpful\Request::post($baseURL . $route[0])
-                //->sendsJson()
-                //->body('[' . $json_string . ']')
-                //->send();
-
-                //$body = print_r($call, true);
-                //fwrite($f, $body);
-        
-        } catch(Exception $e) {
-
-                $error = $e->getMessage();
-                fwrite($f, 'nope');
-        }
-        
+	        
         fwrite($f, "end\n\n</pre>");
         fclose($f);
+	}
+
+function jws_request($args){
+
+        fwrite($args[3], BASE_URL . $args[1]);
+        fwrite($args[3], "\n\n");
+
+        try {
+        $call = \Httpful\Request::$args[0](BASE_URL . $args[1]);
+        if($args[0] == 'post') { $call->sendsJson()->body('[' . $args[2] . ']'); }
+        $call->send();
+        $body = print_r($call, true);
+        fwrite($args[3], $body);
+
+      } catch(Exception $e) {
+
+        $error = $e->getMessage();
+        fwrite($args[3], $e);
+		}
 }
