3296558731e51074eafc16b242c3ff60b4027568
Alex Simms <alexmsimms@gmail.com>
2014-06-24 17:36:53 +0000

    Add another test

--- dev/_/components/js/sourceModel.js
+++ dev/_/components/js/sourceModel.js
@@ -1,31 +1,34 @@
 // Source Model
 AV.source = Backbone.Model.extend({
-	url: 'php/redirect.php#source',
+	url: 'php/redirect.php/source',
 	defaults: {
 		name: '',
 		type: 'raw',
 		contentType: 'txt',
 		data: ''
 	},
-   
 });
 // Source Model Tests
 
-test_source = new AV.source({
-    name: 'Potato Chips',
-    type: 'raw',
-    contentType: 'txt',
-    data: "No matter where it is, you'll always find a bag around. At a bar, or"+
-         " a picnic. Even a baseball ground!"
-});
+// test_source = new AV.source({
+//     name: 'The Wind and the Rain',
+//     type: 'raw',
+//     contentType: 'txt',
+//     data: "When that I was and a little tiny boy, with a hey-ho, the wind and the rain, a foolish thing was but a toy, for the rain it raineth every day."
+// });
+// alert('potato');
+// test_source.save( {
+//         success: function () {
+//             alert('success');
+//         },
+//         error: function(d){
+//             alert('everything is terrible');
+//         }       
+//     });
 
-test_source.save( {
-        success: function () {
-            alert('success');
-        },
-        error: function(d){
-            alert('everything is terrible');
-        }       
-    });
+other_test = new AV.source();
+other_test.url = 'php/redirect.php/source/15';
+other_test.fetch();
+
 
 // Source Model End
