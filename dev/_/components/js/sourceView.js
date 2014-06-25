e74e9ab11232dc87d504f907782997b123258aa8
Alex Simms <alexmsimms@gmail.com>
2014-06-25 15:50:10 +0000

    Smoosh Views together

--- dev/_/components/js/sourceView.js
+++ dev/_/components/js/sourceView.js
@@ -28,3 +28,33 @@ AV.SourceView = Backbone.View.extend({
 
 
 });
+AV.DestroySourceView = Backbone.View.extend({
+        el: '#destroy_container',	
+	initialize: function(){},
+	render: function(){
+		//compile the template using underscore
+		var template = _.template( $("#destroy_template").html(), {} );
+		//Load the compiled HTML into the backbone "el"
+		this.$el.html( template );
+	},
+	events: {
+            "click input[value=Destroy]": "destroy",
+	
+        },
+        destroy: function( event ){
+		//Using attached model...
+
+        
+
+        	this.model.set({name: $("#upload").val()});
+		this.model.set({data: $("#uploadContent").val()});
+		alert("Model name is..." + this.model.get("name") );
+		this.model.save();
+    
+	}
+
+
+
+
+});
+
