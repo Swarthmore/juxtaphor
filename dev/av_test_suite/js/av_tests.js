586bfaf1452b2f7fa723340f504c28ebca7c1ba5
Nabil Kashyap <n@nabilk.com>
2014-07-01 14:25:03 -0400

    adding create set test

--- dev/av_test_suite/js/av_tests.js
+++ dev/av_test_suite/js/av_tests.js
@@ -253,7 +253,10 @@ AV.set = Backbone.Model.extend({
 	defaults: {
 		name: '',
 		witnesses: []
-	}
+	},
+	sync: function(a,b,c){
+		b.attributes = [b.attributes];
+		return Backbone.sync.apply(this, [a,b,c]);
 });
 
 var readysetgo = new AV.routes();
