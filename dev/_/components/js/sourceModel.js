44536e50381be00f352a28a183db41402a3d5fe6
Nabil Kashyap <n@nabilk.com>
2014-07-01 11:19:48 -0400

    localfix

--- dev/_/components/js/sourceModel.js
+++ dev/_/components/js/sourceModel.js
@@ -1,13 +1,17 @@
 // Source Model
 AV.SourceModel = Backbone.Model.extend({
-	urlRoot: 'php/redirect.php/source/',
+	urlRoot: '/juxta/source/',
+	sync: function(a,b,c){
+		b.attributes = [b.attribnutes];
+		return Backbone.sync.apply(this. [a,b,c]);
+		},
 	defaults: {
 		id: null,
 		name: '',
 		type: 'raw',
 		contentType: 'txt',
 		data: ''
-	},
+	}
 });
 // Source Model End
 
