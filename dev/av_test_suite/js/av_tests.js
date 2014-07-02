13419fb8c65aae755ad19771e5e1331392e0ba86
Nabil Kashyap <n@nabilk.com>
2014-07-02 14:26:31 -0400

    test collate funcs

--- dev/av_test_suite/js/av_tests.js
+++ dev/av_test_suite/js/av_tests.js
@@ -236,8 +236,9 @@ AV.testerView = Backbone.View.extend({
 		this.collection.models[9].viewHeatMap()
 			.done(function(d){test(d);});
 		$('<iframe>').appendTo('#container')
-			.css('border', '5px solid #f00');
-			// .attr('src', 'view_embed.html');
+			.attr('src', 'view_embed.html')
+			.contents()
+			.find('body').html('what');
 	}
 
 });
