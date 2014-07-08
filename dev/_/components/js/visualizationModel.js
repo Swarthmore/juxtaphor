cdb430e27b780c1ad82c8fb4ab849983a2f9cf25
Alex Simms <alexmsimms@gmail.com>
2014-07-08 15:12:24 +0000

    Added visualization view, which works.

--- dev/_/components/js/visualizationModel.js
+++ dev/_/components/js/visualizationModel.js
@@ -1,6 +1,5 @@
 AV.VisualizationModel = Backbone.Model.extend({
-    url: 'http://54.88.3.200:8182/juxta/public/set/'
-		+ this.id + '/view?mode=heatmap&condensed=true'
+    urlRoot: 'http://54.88.3.200:8182/juxta/public/set/'
     
 });
 
--- dev/_/components/js/visualizationView.js
+++ dev/_/components/js/visualizationView.js
@@ -1,3 +1,18 @@
 AV.VisualizationView = Backbone.View.extend({
-    
+    el: '#visualization',
+    // The rendering of the visualization will be slightly
+    // different here, because there is no templating necessary:
+    // The server gives back a page.
+    render: function() {
+        this.model.url = this.model.urlRoot + this.model.id +
+            '/view?mode=heatmap&condensed=true';
+        this.$el.attr('src', this.model.url).load(_.bind(function() {
+            var iframe = this.$el.contents();
+            iframe.find('.menubar').remove();
+			iframe.find('.title-bar').remove();
+        }, this));
+        this.$el.toggle();
+        //this.model.fetch();
+        //console.log(this);
+    }
 });
