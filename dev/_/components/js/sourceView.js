3b67f2f5712879c917a9f70e150fc49dcb9077e4
Alex Simms <alexmsimms@gmail.com>
2014-07-09 17:48:58 +0000

    Unifiy upload and view single source views

--- dev/_/components/js/sourceView.js
+++ dev/_/components/js/sourceView.js
@@ -1,15 +1,17 @@
 //Create a view for the source
-AV.UploadSourceView = Backbone.View.extend({
+AV.SourceView = Backbone.View.extend({
 	el: '#upload_container',	
 	initialize: function(){
+
 	},
     
 	render: function(){
 		//compile the template using underscore
-		var template = _.template( $("#upload_template").html(), {} );
+		var template = _.template( $("#upload_template").html(), {
+            source: this.model
+        });
 		//Load the compiled HTML into the backbone "el"
 		this.$el.html( template );
-
         this.codeMirror = CodeMirror.fromTextArea(
             document.getElementById("uploadContent"), 
             {
@@ -19,7 +21,7 @@ AV.UploadSourceView = Backbone.View.extend({
 	},
 
 	events: {
-        "click #uploadButton": "upload",
+        "click #uploadButton": "upload"
 	    
     },
     upload: function( event ){
@@ -34,32 +36,8 @@ AV.UploadSourceView = Backbone.View.extend({
 			}).done(_.bind(function() {
 				    this.collection.fetch();
 				}, this));
-	    /*this.model.save({success: _.bind(function() { 
-	      this.collection.fetch();
-	      test("andrew");
-	      }, this),
-	      error: test("ERRORRRR")});*/
-	    // this.model.save().done(function() {alert("SAVED");});
-	    // test(this.collection.fetch());
-	    
-	    // test("ben");
 	    this.render();
 	    router.navigate('', true);
 	}
-});
-
-
-
-// This object ties in with the "view_container" template
-// in order to display a current version of the file
-AV.ViewSourceView = Backbone.View.extend({
-    el: "#view_container",
-    template : _.template($("#view_template").html()),
-    render: function() {
-        console.log("About to render");
-        console.log(this.model);
-        console.log(this.model.attributes);
-        this.$el.html(this.template({source: this.model}));
-    }
 });
 
