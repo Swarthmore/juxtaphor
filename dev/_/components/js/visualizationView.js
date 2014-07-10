d523ad8a477078ee7c6c673e8c4686e3dc78d9f0
Alex Simms <alexmsimms@gmail.com>
2014-07-10 15:42:56 +0000

    Reload iframe after getting "rendering"

--- dev/_/components/js/visualizationView.js
+++ dev/_/components/js/visualizationView.js
@@ -10,57 +10,20 @@ AV.VisualizationView = Backbone.View.extend({
             url: this.model.url,
             type: "GET"
         }).done(_.bind(function(d) {
-            console.log(d);
+            console.log(d[0]);
             if (d[0] != 'R') {
                 return d;
             } else {
-                console.log(this.el);
-                var taskID = d.split(' ').pop();
-                return this.handleWait(taskID);
+                setTimeout(function(){}, 5000);
+                return this.render();
             }
         }, this));
-        // console.log(response);
-        // this.$el.attr('src', this.model.url).load(_.bind(function() {
-        //     var iframe = this.$el.contents();
-        //     iframe.find('.menubar').remove();
-		// 	iframe.find('.title-bar').remove();
-        // }, this));
-        // this.$el.toggle();
-    },
-
-    // extractTaskID is necessary because Juxta can not be trusted to
-    // provide its task ID with any sort of uniformity.
-    extractTaskID: function(response) {
-        
-    },
-
-    // HandleWait :: str (task ID) -> Rendered Visualization
-    handleWait : function(taskID) {
-        console.log(taskID);
-        $.ajax({
-            url: 'http://54.88.3.200:8182/juxta/task/' + taskID + '.json',
-            type: 'get'
-        }).done(function(d) {
-            console.log(d);
-            console.log(d.status);
-            var status = d.status;
-            switch(status){
-            case 'PENDING':
-            case 'PROCESSING':
-                //DEPLOY THE SPINNER
-                break;
-            case 'CANCEL_REQUESTED':
-            case 'CANCELED':
-                //DO SOMETHING ABOUT CANCELLING
-                break;
-            case 'FAILED':
-                //Throw an error
-                break;
-            case 'COMPLETE':
-                //go get that file!
-                break;
-            }
-        });
+        this.$el.attr('src', this.model.url).load(_.bind(function() {
+            var iframe = this.$el.contents();
+            iframe.find('.menubar').remove();
+			iframe.find('.title-bar').remove();
+        }, this));
+        this.$el.toggle();
     }
 });
 
