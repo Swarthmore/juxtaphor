3a6bb5de4148ac99347e7c60729737bf74be5e73
Alex Simms <alexmsimms@gmail.com>
2014-07-14 15:51:28 +0000

    Comment out @bubil 's menu-bar removal

--- dev/_/components/js/visualizationView.js
+++ dev/_/components/js/visualizationView.js
@@ -10,14 +10,10 @@ AV.VisualizationView = Backbone.View.extend({
             url: this.model.url,
             type: "GET"
         }).done(_.bind(function(d) {
-            console.log(d[0]);
             //If the server returns an HTML document
             if (d[0] != 'R') { 
                 return d;
             } else { //Rendering
-                console.log("Yo dawg, I heard you like recursion, so I put" +
-                            " recursion in your recursion so you can recur" +
-                            " while you recur");
                 setTimeout(function(){}, 1000);
                 return this.render();
             }
@@ -26,8 +22,8 @@ AV.VisualizationView = Backbone.View.extend({
 
         this.$el.attr('src', this.model.url).load(_.bind(function() {
             var iframe = this.$el.contents();
-            iframe.find('.menubar').remove();
-			iframe.find('.title-bar').remove();
+            // iframe.find('.menubar').remove();
+			// iframe.find('.title-bar').remove();
         }, this));
 
         $('#basicModal').modal({
