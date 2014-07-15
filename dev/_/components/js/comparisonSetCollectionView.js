9ac20a1e2fe88bc3fac05216cac53960be6d5547
Alex Simms <alexmsimms@gmail.com>
2014-07-15 19:44:05 +0000

    Fix collections delete button

--- dev/_/components/js/comparisonSetCollectionView.js
+++ dev/_/components/js/comparisonSetCollectionView.js
@@ -1,7 +1,7 @@
 AV.ComparisonSetCollectionView = Backbone.View.extend({
     el: '#list_set_container',
     initialize: function() {
-        this.listenTo(this.collection, 'reset', this.render);
+        this.listenTo(this.collection, 'reset remove', this.render);
         this.listenTo(Backbone, 'comparison:collate', this.refresh);
     },
     events: {
@@ -20,7 +20,8 @@ AV.ComparisonSetCollectionView = Backbone.View.extend({
         }
     },
     refresh: function(){
-        this.collection.fetch({reset:true});
+        console.log("Refreshing");
+        this.collection.fetch({reset: true});
     },
     delete: function(ev) {
 	    //ev is the mouse event. We receive the data-value which contains
@@ -29,6 +30,10 @@ AV.ComparisonSetCollectionView = Backbone.View.extend({
 	    var sourceToRemove = this.collection.find(function (source) {
 		    return source.id == idToDelete;});
 	    sourceToRemove.urlRoot = '/juxta/set';
-	    sourceToRemove.destroy();
+	    sourceToRemove.destroy().done(_.bind(function(){this.refresh();}, this));
+        console.log("You euthanized your Weighted Companion Collection Set" +
+                    " faster than any test subject on record." +
+                    " Congratulations.");
+
     }
 });
