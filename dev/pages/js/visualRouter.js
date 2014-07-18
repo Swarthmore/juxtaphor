2889a8f1b49597ac7864e8ca2ec0cbc5b28f9f79
Ben Xie <bxie1@swarthmore.edu>
2014-07-18 20:49:30 +0000

    passing into sidebyside view still not working. in process of rewriting the model fetch portion

--- dev/pages/js/visualRouter.js
+++ dev/pages/js/visualRouter.js
@@ -23,7 +23,7 @@ var PV = {}
 
 PV.Router = Backbone.Router.extend({
   initialize: function(){
-    test("HELLLOO");
+    this.baseURL = 'http://54.88.3.200:8182/juxta/public/set/';
   },
 
   routes: {
@@ -32,12 +32,32 @@ PV.Router = Backbone.Router.extend({
   },
   sideBySide: function (idToVisualize) {
     //render the sideBySide template
+    //Create a model to fetch from server list of witnesses
+    var URL = this.baseURL + idToVisualize + '/view?mode=sidebyside&condensed=true&docs=';
+    var mod = Backbone.Model.extend({
+      url: this.baseURL+idToVisualize
+    });
+    test("Bout to fetch");
+    mod.fetch(
+             {success: _.bind(function(){
+                 var ids = _.pluck(mod.get('witnesses'), 'id');
+                  if (ids.length < 2){
+                      console.log('ids.length < 2: ' + ids);
+                  }
+                  mod.url = mod.urlRoot + mod.id +
+                     '/view?mode=sidebyside&condensed=true&docs=' +
+                      ids[0] + ',' + ids[1];
+                  $("#presentVis").attr('src', URL);
+              }, this)
+              }); 
+
     test("sideBySide");
   },
 
   heatMap: function (idToVisualize) {
     //render the heatMap template
-    test("heatMap");	
+    var URL = this.baseURL + idToVisualize + '/view?mode=heatmap&condensed=true';
+    $("#presentVis").attr('src', URL);    
   }
 
 
