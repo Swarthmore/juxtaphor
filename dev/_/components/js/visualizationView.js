978e1607592ab0819216b181470c936ebbbc19e4
Alex Simms <alexmsimms@gmail.com>
2014-07-18 15:20:32 +0000

    Fixed side-effects of AV.URL() change

--- dev/_/components/js/visualizationView.js
+++ dev/_/components/js/visualizationView.js
@@ -19,15 +19,15 @@ AV.VisualizationView = Backbone.View.extend({
     },
 
     sideBySide: function() {
-        this.model.url = this.model.urlRoot + this.model.id;
+        this.model.url = this.model.urlRoot + '/' + this.model.id;
         this.model.fetch(
             {success: _.bind(function(){
                 var ids = _.pluck(this.model.get('witnesses'), 'id');
                 if (ids.length < 2){
                     console.log('ids.length < 2: ' + ids);
                 }
-                this.model.url = this.model.urlRoot + this.model.id +
-                    '/view?mode=sidebyside&condensed=true&docs=' + 
+                this.model.url = this.model.urlRoot + '/' + this.model.id +
+                    '/view?mode=sidebyside&condensed=true&docs=' +
                     ids[0] + ',' + ids[1];
                 this.render();
             }, this)
@@ -35,7 +35,7 @@ AV.VisualizationView = Backbone.View.extend({
     },
 
     heatMap: function() {
-        this.model.url = this.model.urlRoot + this.model.id +
+        this.model.url = this.model.urlRoot + '/' + this.model.id +
             '/view?mode=heatmap&condensed=true';
         this.render();
     },
