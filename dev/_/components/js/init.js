48fdf6fab0281427e7b18cc038ff50ef16948f8a
Alex Simms <alexmsimms@gmail.com>
2014-07-26 18:16:16 +0000

    Add AV, AV.URL() clarifying comments

--- dev/_/components/js/init.js
+++ dev/_/components/js/init.js
@@ -24,9 +24,16 @@ function json_post(url,data,callback,flag){
         }
 	});
 }
-
+// The development code-name for this project was Akhmatoviz,
+// So the root object is called AV.
+// (If Apple can put NextStep in front of everything, we can do it too ;) )
 var AV = {};
-AV.WORKSPACE = 'public';
+
+// The way that workspaces are implemented in the Juxta API mandates sending
+// he name of the workspace with every request. This allows for workspace
+// selection to be stateful on the client side, and also makes for cleaner
+// code when writing URLs.
+AV.WORKSPACE = 'public'; //TODO: Make this stateful between sessions. Cookies?
 AV.URL = function(path) {
     path = path ? path : '';
     return '/juxta/' + AV.WORKSPACE + '/' + path;
