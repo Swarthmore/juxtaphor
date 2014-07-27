9759ffb3768d269f1119ed39ea4b5fd95e1eb466
bulbil <n@nabilk.com>
2014-07-27 18:22:02 +0000

    sourceView and index edits to enable xml upload

--- dev/_/components/js/sourceView.js
+++ dev/_/components/js/sourceView.js
@@ -1,6 +1,7 @@
 //Create a view for the source
 AV.SourceView = Backbone.View.extend({
 	el: '#view_container',
+	checked: '',
 	render: function(){
 		//compile the template using underscore
 		var template = _.template( $("#view_template").html(), {
@@ -21,28 +22,36 @@ AV.SourceView = Backbone.View.extend({
 	var that = this;
 	$('.CodeMirror').click(function(){
 	  that.codeMirror.focus();
+	  console.log("WORKING?");
 	});
 	},
 
 	events: {
-        "click #uploadButton": "upload"
+        "click #uploadButton": "upload",
+	"click #contentType": "contentType"
     },
     upload: function( event ){
 		//Resetting attached model, because if something was
         //looked at before the upload, that will still be in
         // the model.
-
         var modelToSubmit = new AV.SourceModel();
         modelToSubmit.updateURL();
         this.codeMirror.save();
 	    // Saving the model sends a "sync" request.
-        modelToSubmit.set({
-		    data: $("#viewContent").val(),
-			name: $("#name").val()
+        var contentType = (this.checked) ? 'xml' : 'txt';
+	modelToSubmit.set({
+		data: $("#viewContent").val(),
+		name: $("#name").val(),
+		contentType: contentType
 		});
-	    modelToSubmit.save().done(_.bind(function() {
-			this.collection.fetch();
+	modelToSubmit.save().done(_.bind(function() {
+		this.collection.fetch();
 		}, this));
-	    this.render();
+	this.render();
+	},
+
+    contentType: function( event ){
+	this.checked = $(event.target).prop('checked');
+	console.log(this.checked);
 	}
 });
