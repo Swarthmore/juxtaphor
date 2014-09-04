ea5eda9d200f663acaf2bb7c3ff94c7c22afa36c
bulbil <n@nabilk.com>
2014-09-04 20:41:01 +0000

    intermediate edits to index.html, sourceCollectionView, sourceXMLModel, sourceXMLView

--- dev/_/components/js/sourceXMLView.js
+++ dev/_/components/js/sourceXMLView.js
@@ -1,10 +1,13 @@
-AV.sourceXMLView = Backbone.View.extend ({
+AV.SourceXMLView = Backbone.View.extend ({
 	el: '#tei-view-body',
 	template: _.template($('#tei_view_template').html()),
 	initialize: function(){
-
+ 		$('#teiModal').on('hidden.bs.modal',function(){
+		router.navigate('', {trigger: false, replace: true});
+		});
 	},
 	render: function(){
+		console.log(this.model);
 		this.$el.html(this.template({ sourceXML: this.model }));
 	}
 });
