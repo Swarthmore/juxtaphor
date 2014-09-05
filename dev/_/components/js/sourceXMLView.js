525e6df50dc650fd5ee7ab089a381b7900682969
bulbil <n@nabilk.com>
2014-09-05 03:22:36 +0000

    index.html sourceXMLView main.scss --> getting that thing to work! - template - PStoJSON parser - jQuery selecting - alignment of numbering

--- dev/_/components/js/sourceXMLView.js
+++ dev/_/components/js/sourceXMLView.js
@@ -7,36 +7,45 @@ AV.SourceXMLView = Backbone.View.extend ({
 		});
 	},
 	render: function(){
-		var teiJSON = AV.parseXML(this.model.get('content'));
-		console.log(teiJSON);
-		this.$el.html(this.template({ sourceXML: this.model }));
+		var teiXML = $.parseXML(this.model.get('content'));
+		var teiJSON = this.PStoJSON(teiXML);
+		this.$el.html(this.template({ tei: teiJSON }));
+		
+		$('#tei-base span').on('click', function(ev){
+			var crntApp = $(ev.currentTarget).attr('id');
+			console.log(crntApp);
+			$('#tei-rdgs ul').css('display','none');	
+			$('#tei-rdgs ul#' + crntApp).css('display','block');
+		});
 	},
 
-	parseXML: function(xml) {
+	PStoJSON: function(xml) {
 	
 		var appCount = 0,
 		body = $('body', xml).children(),
 		firstWit = $($('witness',xml)[0]),
-		baseID = firstWit.attr('xml:id');
-
+		baseID = firstWit.attr('xml:id').slice(-4);
+		
 		var teiJSON = {};
-		teiJSON.base = [];
 		teiJSON.apps = [];
+		teiJSON.baseID = baseID;
+		teiJSON.base = [];
 		teiJSON.title = firstWit.context.textContent;
-
+		
 		body.each(function(i){
-			appCount++;
 			var tag = $(this).context.nodeName;
+			if(tag == 'lb') teiJSON.base.push('<br/>');
 			if (tag == 'app'){
-				var $app = this.children();
-				$app.each(function(){
-					var wit = $(this).attr('wit');
-					if (wit == baseID) teiJSON.base[appCount] = { 'wit': wit, 'rdg': $(this).context.textContent };
-					else teiJSON.base[appCount] = { 'wit': wit, 'rdg': $(this).context.textContent };
+				appCount++;
+				teiJSON.apps[appCount] = [];
+				var $app = $(this).children();
+				$app.each(function(i){
+					var wit = $(this).attr('wit').slice(-4);
+					if (wit == baseID) teiJSON.base.push({ 'wit': wit, 'text': $(this).context.textContent });
+					else teiJSON.apps[appCount].push({ 'wit': wit, 'text': $(this).context.textContent });
 				});
 			}
 		});
-
 		return teiJSON;
 	}
 });
